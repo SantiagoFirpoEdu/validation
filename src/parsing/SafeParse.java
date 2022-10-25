5eff58d71c8f5e45f981e42598f9468c62f3537e
Santiago Firpo <s.firpo@edu.pucrs.br>
2022-10-25 01:30:16 -0300

    Added action implementations

--- src/parsing/SafeParse.java
+++ src/parsing/SafeParse.java
@@ -1,6 +1,6 @@
 package parsing;
 
-import result.Result;
+import result.GResult;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -8,27 +8,27 @@ import java.text.ParseException;
 public class SafeParse
 {
 	private SafeParse() {}
-	public static Result<Integer, NumberFormatException> safeParseInt(String input)
+	public static GResult<Integer, NumberFormatException> safeParseInt(String input)
 	{
 		try
 		{
-			return Result.ok(Integer.parseInt(input));
+			return GResult.ok(Integer.parseInt(input));
 		}
 		catch (NumberFormatException exception)
 		{
-			return Result.error(exception);
+			return GResult.error(exception);
 		}
 	}
 
-	public static Result<Double, Exception> safeParseDouble(String input)
+	public static GResult<Double, Exception> safeParseDouble(String input)
 	{
 		try
 		{
-			return Result.ok(NumberFormat.getInstance().parse(input).doubleValue());
+			return GResult.ok(NumberFormat.getInstance().parse(input).doubleValue());
 		}
 		catch (NumberFormatException | ParseException exception)
 		{
-			return Result.error(exception);
+			return GResult.error(exception);
 		}
 	}
 }
